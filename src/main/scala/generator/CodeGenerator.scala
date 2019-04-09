a0e75d893683de1bda94561fba3e1e4233ef1836
Jacob Gagne <gagne.jacob@gmail.com>
2019-04-09 03:09:25 -0400

    added basic variable assignment instructions

--- src/main/scala/generator/CodeGenerator.scala
+++ src/main/scala/generator/CodeGenerator.scala
@@ -3,6 +3,7 @@ package generator
 import java.io.{File, PrintWriter}
 
 import parser.ASTNode
+import generator.GeneratorTraversers._
 
 object CodeGenerator {
 
@@ -11,13 +12,30 @@ object CodeGenerator {
 
     writer.write(f"${" "}%-15s entry %% Start here\n")
 
-    writer.write(f"${" "}%-15s htl   %% Stop here\n")
+    createFunctionsAndMain(symbolTableWithMemoryOffsets, ast, writer)
+
+    writer.write(f"${" "}%-15s hlt   %% Stop here\n")
 
     allocateMemory(symbolTableWithMemoryOffsets, writer)
 
     writer.close()
   }
 
+  /**
+    * Creates function blocks in moon code
+    * @param table symbol table containing memory information
+    * @param node root node for ast tree
+    * @param writer program file writer
+    */
+  def createFunctionsAndMain(table: SymbolMemoryTable, node: ASTNode, writer: PrintWriter): Unit = {
+    traverseMain(table, node, writer)
+  }
+
+  /**
+    * Allocate top level memory used by the main block
+    * @param table symbol table containing memory requirements
+    * @param writer program file writer
+    */
   def allocateMemory(table: SymbolMemoryTable, writer: PrintWriter): Unit = {
     table.symbols.find(s => s.kind.equals("function") && s.name.equals("main")).fold()(l => {
       l.link.fold()(t => {
